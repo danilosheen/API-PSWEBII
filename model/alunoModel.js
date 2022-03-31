c826a655073b9de38796d49133f7359cfa21e6fc
danilosheen <c.danilo.f.silva@gmail.com>
2022-03-31 17:43:54 -0300

    update

--- model/alunoModel.js
+++ model/alunoModel.js
@@ -4,26 +4,26 @@ const con = require('./conexao');
 const jwt = require('jsonwebtoken')
 
 const login = (request, response) => {
-    if (req.body.user === process.env.USUARIO && req.body.pass === process.env.SENHA) {
+    if (request.body.user === process.env.USUARIO && request.body.pass === process.env.SENHA) {
         const id = 1;
         var token = jwt.sign({ id }, process.env.APP_KEY, { expiresIn: 300 });
-        res.set("x-access-token", token);
-        res.json({ auth: true, token: token });
+        response.set("x-access-token", token);
+        response.json({ auth: true, token: token });
     } else {
-        res.status(500).json({ mensagem: 'Login Inválido' });
+        response.status(500).json({ mensagem: 'Login Inválido' });
     }
 }
 
 function verifyJWT (request, response, next){
-    let token = req.headers['x-access-token'];
+    let token = request.headers['x-access-token'];
     if (!token){
-        return res.status(401).json({ auth: false, mensagem: 'Sem token de verificação'});
+        return response.status(401).json({ auth: false, mensagem: 'Sem token de verificação'});
 
     }
 
     jwt.verify(token, process.env.APP_KEY, function(error, decoded){
         if (error){
-            return res.status(500).json({ mensagem: 'Token inválido'});
+            return response.status(500).json({ mensagem: 'Token inválido'});
         }
         next();
     });
