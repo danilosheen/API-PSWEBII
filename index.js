755df30f041135bc5e3460443a0fd3c955aa5b0c
danilosheen <c.danilo.f.silva@gmail.com>
2022-03-31 16:37:38 -0300

    teste

--- index.js
+++ index.js
@@ -1,92 +1,13 @@
 const express = require('express');
 const app = express();
-const jwt = require('jsonwebtoken');
-const {obterAlunos} = require('./model/alunoModel');
-require('dotenv').config()
 
+const repository = require('./model/alunoModel');
 app.use(express.json());
 
-app.post('/login', (req, res) => {
-    if (req.body.user === process.env.USUARIO && req.body.pass === process.env.SENHA) {
-        const id = 1;
-        var token = jwt.sign({id}, process.env.APP_KEY, {expiresIn: 300});
-        res.set("x-access-token", token);
-        res.json({auth: true, token: token});
-    } else {
-        res.status(500).json({mensagem: 'Login inválido'});
-    }
-
-    
-});
-
-function verifyJWT (req, res, next) {
-    let token = req.headers['x-access-token'];
-    if (!token) {
-        return res.status(401).json({auth: false, mensagem: 'Sem token de verificação'});
-    }
-
-    jwt.verify(token, process.env.APP_KEY, function (error, decoded) {
-        if (error) {
-            return res.status(500).json({mensagem: 'Token inválido'});
-        }
-        next();
-    });
-}
-
-
-app.get('/alunos', async (req, res) => {
-    try{
-        const resultado = await obterAlunos(req, res);
-        const dados = await resultado.rows;
-        res.json(dados);
-    } catch(erro) {
-        res.json({'mensagem': 'Erro na obteção dos dados'})
-    }
-
-});
-
-app.get('/alunos/:id', async (req, res) => {
-    
-    try{
-        const resultado = await obterAluno(req, res);
-        const dados = await resultado.rows;
-        res.json(dados);
-    } catch(erro) {
-        res.json({'mensagem': 'Erro na obteção dos dados'})
-    }
-     
-    });
-
-app.post('/alunos', verifyJWT, (req, res, next) => {
-    dados.push(req.body);
-    res.json({
-        mensagem: 'Informação gravada com sucesso',
-        erro: false
-    });
-});
-
-app.put('/alunos', (req, res) => {
-    dados.forEach( (objeto, indice) => {
-        if (objeto.id == req.body.id) {
-            dados[indice] = req.body;
-        } 
-    });
-    res.json({
-        mensagem: 'Informação atualizada',
-        erro: false
-    });
-});
-
-app.delete('/alunos', (req, res) => {
-    dados.forEach( (objeto, indice) => {
-        if (objeto.id == req.body.id) {
-            dados.splice(indice);
-        }
-    });
-    res.json({
-        mensagem: 'Informação excluída com sucesso',
-        erro: false
-    });
-});
+app.get('/pessoas', repository.getPessoas)
+app.get('/pessoas/:id', repository.getPessoaById)
+app.post('/pessoas', repository.createPessoa)
+app.put('/pessoas/:id', repository.updatePessoa)
+app.delete('/pessoas/:id', repository.deletePessoa)
 
 app.listen(process.env.PORT);
